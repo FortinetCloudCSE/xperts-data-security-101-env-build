ecfcdcdc21447942d294b03782c56da0bd5f75b9
John McDonough <movinalot@gmail.com>
2025-10-14 18:09:15 -0400

    add variable defaults

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,33 +1,40 @@
 variable "username_prefix" {
   description = "Prefix for the username"
   type        = string
+  default = ""
 }
 
 variable "user_count" {
   description = "Number of users to create"
   type        = string
+  default = "0"
 }
 variable "user_start" {
   description = "Starting index for user numbering"
   type        = string
+  default = "0"
 }
 
 variable "rg-suffix" {
   description = "Suffix for the resource group name"
   type        = string
+  default     = ""
 }
 
 variable "location" {
   description = "Azure location for resources"
   type        = string
+  default     = ""
 }
 
 variable "password" {
   description = "Password for the admin user"
   type        = string
+  default     = ""
 }
 
 variable "subscription_id" {
   description = "Azure Subscription ID"
   type        = string
+  default     = ""
 }
